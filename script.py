c7e479e0feeae7eb3f3188898cf21c1a20ff6847
leorjr <l.grodriguesjr@gmail.com>
2022-02-15 21:26:12 -0300

    feat: created a function for validation a password

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import re
+
 def create_a_ladder():
 
     while True:
@@ -12,4 +14,30 @@ def create_a_ladder():
         print('*'*item)
 
 
-create_a_ladder()
+
+def validation_on_a_password():
+
+    while True:
+
+        password = input("Informe o password: ")
+
+        if len(password) < 6:
+            print('Password precisa ter, no mínimo, 6 caracteres')
+
+        elif re.search(r'\d', password) == None:
+            print('Favor informar, ao menos, 1 número.')
+
+        elif re.search(r'[A-Z]', password) == None:
+            print('Favor informar, ao menos, 1 letra maiúscula.')
+        
+        elif re.search(r'[a-z]', password) == None:
+            print('Favor informar, ao menos, 1 letra minúscula.')
+        
+        elif re.search(r'[!@#$%^&*()\-+]', password) == None:
+            print('Favor informar, ao menos, 1 caractere especial.')
+        
+        else:
+            print('Password validado!')
+            break
+            
+       
